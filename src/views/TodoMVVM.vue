d86b7b7565a45ccef4f6501d8a5c70523091d60a
Ringo Yungpo Kao <ringoyungpo@qq.com>
2019-01-26 13:46:51 +0800

    add input function and format everything

--- src/views/TodoMVVM.vue
+++ src/views/TodoMVVM.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="todoMVVM">
-    <h1>This is an todoMVVM page</h1>
-  </div>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo"
+             autofocus
+             autocomplete="off"
+             placeholder="What needs to be done?"
+             v-model="newTodo">
+    </header>
+    <p>{{newTodo}}</p>
+    <footer class="info">
+      <p>Double-click to edit a todo</p>
+      <p>Written by <a href="http://evanyou.me">Evan You</a></p>
+      <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+    </footer>
+  </section>
 </template>
+
+<script lang="ts">
+import { Component, Provide, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class TodoMVVM extends Vue {
+  @Provide() private newTodo: string = ''
+}
+</script>
+
