7c43cb3d98be7aa0245d6de0f00023219856bd49
Ringo Yungpo Kao <ringoyungpo@qq.com>
2019-01-26 14:30:21 +0800

    able to add a item

--- src/views/TodoMVVM.vue
+++ src/views/TodoMVVM.vue
@@ -6,12 +6,14 @@
              autofocus
              autocomplete="off"
              placeholder="What needs to be done?"
-             v-model="newTodo">
+             v-model="newTodo"
+             @keyup.enter="addTodo">
     </header>
     <p>{{newTodo}}</p>
+    <p>{{todos}}</p>
     <footer class="info">
       <p>Double-click to edit a todo</p>
-      <p>Written by <a href="http://evanyou.me">Evan You</a></p>
+      <p>Written by <a href="http://ringoyungpo.github.io">Ringo Yungpo Kao</a></p>
       <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
     </footer>
   </section>
@@ -23,6 +25,16 @@ import { Component, Provide, Vue } from 'vue-property-decorator'
 @Component({})
 export default class TodoMVVM extends Vue {
   @Provide() private newTodo: string = ''
+  @Provide() private todos: string[] = []
+
+  private addTodo() {
+    const value = this.newTodo && this.newTodo.trim()
+    if (!value) {
+      return
+    }
+    this.todos.push(value)
+    this.newTodo = ''
+  }
 }
 </script>
 
